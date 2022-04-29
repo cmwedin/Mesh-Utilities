bfb04802ffc517934aced378815ce0cf99c14df7
cmwedin <cmwedin@gmail.com>
2022-04-29 16:09:05 -0400

    packaging
    rerooted the repo to be a proper package now that the basic features are working

--- Runtime/MeshUtilities.cs
+++ Runtime/MeshUtilities.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MeshUtilities {
+namespace SadSapphicGames.MeshUtilities {
     public class MeshUtilities {
         private Mesh _mesh;
         MeshCollider meshCollider;
