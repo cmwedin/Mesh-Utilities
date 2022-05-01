9477613b85aeaa3b6c746bfa49b611ca25e81068
cmwedin <cmwedin@gmail.com>
2022-05-01 09:14:24 -0400

    list length error control
    an exception now occurs if some colors/UVs are set but not enough to cover the whole mesh

--- Runtime/MeshUtilityWrapper.cs
+++ Runtime/MeshUtilityWrapper.cs
@@ -58,7 +58,7 @@ namespace SadSapphicGames.MeshUtilities {
             newUV.Add(v1);
             newUV.Add(v2);
             newUV.Add(v3);
-
+            CheckListLength(newUV);
         }
         public void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
         {
@@ -77,40 +77,76 @@ namespace SadSapphicGames.MeshUtilities {
             newTriangles.Add(vertIndex + 2);
             newTriangles.Add(vertIndex + 3);
         }
+        public void AddQuadUV(Vector2 v1, Vector2 v2, Vector2 v3, Vector2 v4) {
+            newUV.Add(v1);
+            newUV.Add(v2);
+            newUV.Add(v3);
+
+            newUV.Add(v1);
+            newUV.Add(v3);
+            newUV.Add(v4);
+            CheckListLength(newUV);
+        }
         public void AddLineSegment(Vector3 v1, Vector3 v2, Vector3 brush) { //? QOL function, this could easily be made as a quad
             AddQuad(v1 - brush, v1 + brush, v2 + brush, v2 - brush);
         }
 
         // * Adding Vertex Colors
         public void AddQuadColor(Color c1) {
+            if(!useVertColors) return;
             newColors.Add(c1);
             newColors.Add(c1);
             newColors.Add(c1);
             newColors.Add(c1);
-
+            CheckListLength(newColors);
         }
         public void AddQuadColor(Color c1, Color c2) {
+            if(!useVertColors) return;
             newColors.Add(c1);
             newColors.Add(c1);
             newColors.Add(c2);
             newColors.Add(c2);
-
+            CheckListLength(newColors);
         }
         public void AddQuadColor(Color c1, Color c2, Color c3, Color c4) {
+            if(!useVertColors) return;
             newColors.Add(c1);
             newColors.Add(c2);
             newColors.Add(c3);
             newColors.Add(c4);
+            CheckListLength(newColors);
         }
         public void AddTriangleColor(Color color) {
+            if(!useVertColors) return;
             newColors.Add(color);
             newColors.Add(color);
             newColors.Add(color);
+            CheckListLength(newColors);
         }
         public void AddTriangleColor(Color c1, Color c2, Color c3) {
+            if(!useVertColors) return;
             newColors.Add(c1);
             newColors.Add(c2);
             newColors.Add(c3);
+            CheckListLength(newColors);
         }
+        private void CheckListLength(List<> list) {
+            if (
+                list.Count != 0
+                && list.Count != newTriangles.Count //? i believe this is the correct comparison to make
+            ) {
+                throw new InvalidListLengthException($"{list.Name} isn't empty but does not match the length of newTriangles, are you missing a function call somewhere?");
+            } else return;
+        }
+    }
+    [System.Serializable]
+    public class InvalidListLengthException : System.Exception
+    {
+        public InvalidListLengthException() { }
+        public InvalidListLengthException(string message) : base(message) { }
+        public InvalidListLengthException(string message, System.Exception inner) : base(message, inner) { }
+        protected InvalidListLengthException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
